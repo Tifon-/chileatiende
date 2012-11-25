016e173a9cf3eccfb0ca4ec1d9c47705812235cc
Nicolás Moncada <nicolas.moncada@tifon.cl>
2012-11-25 00:36:53 -0300

    Primer Commit del módulo

---
+++ chileatiende-tips.tpl.php
@@ -0,0 +1,57 @@
+<?php print t('Ejemplos para imprimir datos de ChileAtiende')?>:
+<table>
+	<thead>
+		<tr>
+			<th>Fichas</th>
+			<th>Servicios</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class="odd">
+			<td>
+				Si desea imprimir el título de una ficha, puede escribir [chileatiende-ficha:AN998-2 titulo]
+			</td>
+			<td>
+				Si desea imprimir el nombre de un servicio, puede escribir [chileatiende-servicio:AC004 nombre]
+			</td>
+		</tr>
+		<tr class="even">
+			<td>
+				<strong>Lista de datos para usar en ficha</strong>
+				<ul>
+					<li>codigo</li>
+					<li>fecha</li>
+					<li>servicio</li>
+					<li>titulo</li>
+					<li>objetivo</li>
+					<li>beneficiarios</li>
+					<li>costo</li>
+					<li>vigencia</li>
+					<li>plazo</li>
+					<li>observaciones</li>
+					<li>marco_legal</li>
+					<li>doc_requeridos</li>
+					<li>guia_online</li>
+					<li>guia_online_url</li>
+					<li>guia_oficina</li>
+					<li>guia_telefonico</li>
+					<li>guia_correo</li>
+					<li>temas</li>
+					<li>tags</li>
+					<li>chileclic_id</li>
+					<li>permalink</li>
+				</ul>
+			</td>
+			<td>
+				<strong>Lista de datos para usar en servicio</strong>
+				<ul>
+					<li>id</li>
+					<li>sigla</li>
+					<li>nombre</li>
+					<li>url</li>
+					<li>mision</li>
+				</ul>
+			</td>
+		</tr>
+	</tbody>
+</table>
